3b42f12a8da430da4d4cdcf55097258f85e2faf9
easylearningbd <elebdsoft@gmail.com>
2022-04-17 01:18:52 +0600

    12  Install Laravel Image Intervention Package

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Intervention\Image\ImageServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -191,7 +191,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // ...
+        'Image' => Intervention\Image\Facades\Image::class,
     ])->toArray(),
 
 ];

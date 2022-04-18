c7ace8a34c4a6be8443cad1e730ac89472688919
easylearningbd <elebdsoft@gmail.com>
2022-04-19 03:18:33 +0600

    34 Purchase Management  Part 7

--- resources/views/backend/purchase/purchase_add.blade.php
+++ resources/views/backend/purchase/purchase_add.blade.php
@@ -74,15 +74,63 @@
 
 
 
-    </div> <!-- // end row  -->
-
-  
-             
-
-
-           
+    </div> <!-- // end row  --> 
            
         </div> <!-- End card-body -->
+<!--  ---------------------------------- -->
+
+        <div class="card-body">
+        <form method="" action="">
+            @csrf
+            <table class="table-sm table-bordered" width="100%" style="border-color: #ddd;">
+                <thead>
+                    <tr>
+                        <th>Category</th>
+                        <th>Product Name </th>
+                        <th>PSC/KG</th>
+                        <th>Unit Price </th>
+                        <th>Description</th>
+                        <th>Total Price</th>
+                        <th>Action</th> 
+
+                    </tr>
+                </thead>
+
+                <tbody id="addRow" class="addRow">
+                    
+                </tbody>
+
+                <tbody>
+                    <tr>
+                        <td colspan="5"></td>
+                        <td>
+                            <input type="text" name="estimated_amount" value="0" id="estimated_amount" class="form-control estimated_amount" readonly style="background-color: #ddd;" >
+                        </td>
+                        <td></td>
+                    </tr>
+
+                </tbody>                
+            </table><br>
+            <div class="form-group">
+                <button type="submit" class="btn btn-info" id="storeButton"> Purchase Store</button>
+                
+            </div>
+            
+        </form>
+
+
+
+
+
+
+        </div> <!-- End card-body -->
+
+
+ 
+
+
+
+
     </div>
 </div> <!-- end col -->
 </div>

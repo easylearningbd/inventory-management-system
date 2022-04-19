42ead2c80409a60456295e7b53e8d5a853e38e21
easylearningbd <elebdsoft@gmail.com>
2022-04-19 15:27:54 +0600

    37 Purchase Management  Part 10

--- resources/views/backend/purchase/purchase_add.blade.php
+++ resources/views/backend/purchase/purchase_add.blade.php
@@ -222,16 +222,49 @@
                  }
 
 
-                 var source = $("document-template").html();
+                 var source = $("#document-template").html();
                  var tamplate = Handlebars.compile(source);
+                 var data = {
+                    date:date,
+                    purchase_no:purchase_no,
+                    supplier_id:supplier_id,
+                    category_id:category_id,
+                    category_name:category_name,
+                    product_id:product_id,
+                    product_name:product_name
 
+                 };
+                 var html = tamplate(data);
+                 $("#addRow").append(html); 
+        });
 
+        $(document).on("click",".removeeventmore",function(event){
+            $(this).closest(".delete_add_more_item").remove();
+            totalAmountPrice();
+        });
 
+        $(document).on('keyup click','.unit_price,.buying_qty', function(){
+            var unit_price = $(this).closest("tr").find("input.unit_price").val();
+            var qty = $(this).closest("tr").find("input.buying_qty").val();
+            var total = unit_price * qty;
+            $(this).closest("tr").find("input.buying_price").val(total);
+            totalAmountPrice();
+        });
 
+        // Calculate sum of amout in invoice 
 
+        function totalAmountPrice(){
+            var sum = 0;
+            $(".buying_price").each(function(){
+                var value = $(this).val();
+                if(!isNaN(value) && value.length != 0){
+                    sum += parseFloat(value);
+                }
+            });
+            $('#estimated_amount').val(sum);
+        }  
 
-        })
-    })
+    });
 
 
 </script>

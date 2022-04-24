2ed9f614e6a6fa41c6857726ce24dcede556821c
easylearningbd <elebdsoft@gmail.com>
2022-04-25 02:40:51 +0600

    54 Invoice Form Add Part 2

--- app/Http/Controllers/Pos/InvoiceController.php
+++ app/Http/Controllers/Pos/InvoiceController.php
@@ -17,6 +17,7 @@ use App\Models\InvoiceDetail;
 use App\Models\Payment;
 use App\Models\PaymentDetail;
 use App\Models\Customer;
+use DB;
 
 class InvoiceController extends Controller
 {
@@ -48,31 +49,68 @@ class InvoiceController extends Controller
 
     public function InvoiceStore(Request $request){
 
-        if ($request->category_id == null) {
+    if ($request->category_id == null) {
+
+       $notification = array(
+        'message' => 'Sorry You do not select any item', 
+        'alert-type' => 'error'
+    );
+    return redirect()->back()->with($notification);
+
+    } else{
+        if ($request->paid_amount > $request->estimated_amount) {
 
            $notification = array(
-            'message' => 'Sorry You do not select any item', 
-            'alert-type' => 'error'
-        );
-        return redirect()->back()->with($notification);
+        'message' => 'Sorry Paid Amount is Maximum the total price', 
+        'alert-type' => 'error'
+    );
+    return redirect()->back()->with($notification);
 
-        } else{
-            if ($request->paid_amount > $request->estimated_amount) {
+        } else {
 
-               $notification = array(
-            'message' => 'Sorry Paid Amount is Maximum the total price', 
-            'alert-type' => 'error'
-        );
-        return redirect()->back()->with($notification);
-        
-            } else {
+    $invoice = new Invoice();
+    $invoice->invoice_no = $request->invoice_no;
+    $invoice->date = date('Y-m-d',strtotime($request->date));
+    $invoice->description = $request->description;
+    $invoice->status = '0';
+    $invoice->created_by = Auth::user()->id; 
 
+    DB::transaction(function() use($request,$invoice){
+        if ($invoice->save()) {
+           $count_category = count($request->category_id);
+           for ($i=0; $i < $count_category ; $i++) { 
 
+              $invoice_details = new InvoiceDetail();
+              $invoice_details->date = date('Y-m-d',strtotime($request->date));
+              $invoice_details->invoice_id = $invoice->id;
+              $invoice_details->category_id = $request->category_id[$i];
+              $invoice_details->product_id = $request->product_id[$i];
+              $invoice_details->selling_qty = $request->selling_qty[$i];
+              $invoice_details->unit_price = $request->unit_price[$i];
+              $invoice_details->selling_price = $request->selling_price[$i];
+              $invoice_details->status = '1'; 
+              $invoice_details->save(); 
+           }
 
+            if ($request->customer_id == '0') {
+                $customer = new Customer();
+                $customer->name = $request->name;
+                $customer->mobile_no = $request->mobile_no;
+                $customer->email = $request->email;
+                $customer->save();
+                $customer_id = $customer->id;
+            } else{
+                $customer_id = $request->customer_id;
+            } 
 
-
-            }
         }
+
+
+            });
+
+
+        }
+    }
 
     } // End Method
 

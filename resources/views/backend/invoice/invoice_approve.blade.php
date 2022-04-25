ce09b98e58cf02f11e3e588b2f8e303c0ef37650
easylearningbd <elebdsoft@gmail.com>
2022-04-26 02:16:54 +0600

    62 Invoice Approval Part 4

--- resources/views/backend/invoice/invoice_approve.blade.php
+++ resources/views/backend/invoice/invoice_approve.blade.php
@@ -45,15 +45,15 @@
      </table>    
 
 
-     <form>
-         
+     <form method="post" action="">
+      @csrf         
          <table border="1" class="table table-dark" width="100%">
             <thead>
                 <tr>
                     <th class="text-center">Sl</th>
                     <th class="text-center">Category</th>
                     <th class="text-center">Product Name</th>
-                    <th class="text-center">Current Stock</th>
+                    <th class="text-center" style="background-color: #8B008B">Current Stock</th>
                     <th class="text-center">Quantity</th>
                     <th class="text-center">Unit Price </th>
                     <th class="text-center">Total Price</th>
@@ -61,22 +61,51 @@
                 
             </thead>
     <tbody>
+        @php
+        $total_sum = '0';
+        @endphp
         @foreach($invoice['invoice_details'] as $key => $details)
         <tr>
             <td class="text-center">{{ $key+1 }}</td>
             <td class="text-center">{{ $details['category']['name'] }}</td>
             <td class="text-center">{{ $details['product']['name'] }}</td>
-            <td class="text-center">{{ $details['product']['quantity'] }}</td>
+            <td class="text-center" style="background-color: #8B008B">{{ $details['product']['quantity'] }}</td>
             <td class="text-center">{{ $details->selling_qty }}</td>
             <td class="text-center">{{ $details->unit_price }}</td>
             <td class="text-center">{{ $details->selling_price }}</td>
         </tr>
+        @php
+        $total_sum += $details->selling_price;
+        @endphp
         @endforeach
+        <tr>
+            <td colspan="6"> Sub Total </td>
+             <td > {{ $total_sum }} </td>
+        </tr>
+         <tr>
+            <td colspan="6"> Discount </td>
+             <td > {{ $payment->discount_amount }} </td>
+        </tr>
+
+         <tr>
+            <td colspan="6"> Paid Amount </td>
+             <td >{{ $payment->paid_amount }} </td>
+        </tr>
+
+         <tr>
+            <td colspan="6"> Due Amount </td>
+             <td > {{ $payment->due_amount }} </td>
+        </tr>
+
+        <tr>
+            <td colspan="6"> Grand Amount </td>
+             <td >{{ $payment->total_amount }}</td>
+        </tr>
     </tbody>
              
          </table>
  
-
+         <button type="submit" class="btn btn-info">Invoice Approve </button>
 
      </form> 
 

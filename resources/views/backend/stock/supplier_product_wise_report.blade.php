f54b735b0a4314aec5f3553dd91f8288f9bc9e03
easylearningbd <elebdsoft@gmail.com>
2022-04-28 05:03:33 +0600

    75 Product Wise Stock Report Part 1

--- resources/views/backend/stock/supplier_product_wise_report.blade.php
+++ resources/views/backend/stock/supplier_product_wise_report.blade.php
@@ -37,7 +37,7 @@
         </div>        
     </div> <!-- // end row  -->
 
-
+<!--  /// Supplier Wise  -->
     <div class="show_supplier" style="display:none">
         <form method="GET" action="{{ route('supplier.wise.pdf') }}" id="myForm" target="_blank" >
 
@@ -61,8 +61,47 @@
         </form>
         
     </div>
+<!--  /// End Supplier Wise  -->
+
+<!--  /// Product Wise  -->
+ <div class="show_product" style="display:none; ">
+        <form method="GET" action="{{ route('supplier.wise.pdf') }}" id="myForm" target="_blank" >
+
+            <div class="row">
+
+               <div class="col-md-4">
+            <div class="md-3">
+                <label for="example-text-input" class="form-label">Category Name </label>
+                <select name="category_id" id="category_id" class="form-select select2" aria-label="Default select example">
+                <option selected="">Open this select menu</option>
+                  @foreach($category as $cat)
+                <option value="{{ $cat->id }}">{{ $cat->name }}</option>
+               @endforeach
+                </select>
+            </div>
+        </div>
 
 
+         <div class="col-md-4">
+            <div class="md-3">
+                <label for="example-text-input" class="form-label">Product Name </label>
+                <select name="product_id" id="product_id" class="form-select select2" aria-label="Default select example">
+                <option selected="">Open this select menu</option>
+               
+                </select>
+            </div>
+        </div>
+
+                <div class="col-sm-4" style="padding-top: 28px;">
+                    <button type="submit" class="btn btn-primary">Search</button>
+                </div>
+                
+            </div>
+            
+        </form>
+        
+    </div>
+<!--  /// End Product Wise  -->
 
 
 
@@ -79,7 +118,26 @@
                     </div> <!-- container-fluid -->
                 </div>
 
+<script type="text/javascript">
+    $(function(){
+        $(document).on('change','#category_id',function(){
+            var category_id = $(this).val();
+            $.ajax({
+                url:"{{ route('get-product') }}",
+                type: "GET",
+                data:{category_id:category_id},
+                success:function(data){
+                    var html = '<option value="">Select Category</option>';
+                    $.each(data,function(key,v){
+                        html += '<option value=" '+v.id+' "> '+v.name+'</option>';
+                    });
+                    $('#product_id').html(html);
+                }
+            })
+        });
+    });
 
+</script>
 
 
 <script type="text/javascript">
@@ -89,6 +147,19 @@
             $('.show_supplier').show();
         }else{
             $('.show_supplier').hide();
+        }
+    }); 
+
+</script>
+
+
+<script type="text/javascript">
+    $(document).on('change','.search_value', function(){
+        var search_value = $(this).val();
+        if (search_value == 'product_wise') {
+            $('.show_product').show();
+        }else{
+            $('.show_product').hide();
         }
     }); 
 

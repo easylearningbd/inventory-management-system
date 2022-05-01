79198d4ed600a58e77481ccbb0b8b9f98a66eca1
easylearningbd <elebdsoft@gmail.com>
2022-05-01 22:15:34 +0600

    94 Update Some Desing In Dashboard

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -2,7 +2,7 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-sm-6">
-                                 EasyLearning © Upcube.
+                                 EasyLearning © Support@easylearningbd.com.
                             </div>
                             <div class="col-sm-6">
                                 <div class="text-sm-end d-none d-sm-block">
--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -12,8 +12,8 @@
                             <li class="menu-title">Menu</li>
 
                             <li>
-                                <a href="index.html" class="waves-effect">
-                                    <i class="ri-dashboard-line"></i> 
+                                <a href="{{ url('/dashboard') }}" class="waves-effect">
+                                    <i class="ri-home-fill"></i> 
                                     <span>Dashboard</span>
                                 </a>
                             </li>
@@ -21,7 +21,7 @@
                 
         <li>
             <a href="javascript: void(0);" class="has-arrow waves-effect">
-                <i class="ri-mail-send-line"></i>
+                <i class="ri-hotel-fill"></i>
                 <span>Manage Suppliers</span>
             </a>
             <ul class="sub-menu" aria-expanded="false">
@@ -33,7 +33,7 @@
 
         <li>
             <a href="javascript: void(0);" class="has-arrow waves-effect">
-                <i class="ri-mail-send-line"></i>
+                <i class="ri-shield-user-fill"></i>
                 <span>Manage Customers</span>
             </a>
             <ul class="sub-menu" aria-expanded="false">
@@ -49,7 +49,7 @@
 
          <li>
             <a href="javascript: void(0);" class="has-arrow waves-effect">
-                <i class="ri-mail-send-line"></i>
+                <i class="ri-delete-back-fill"></i>
                 <span>Manage Units</span>
             </a>
             <ul class="sub-menu" aria-expanded="false">
@@ -60,7 +60,7 @@
 
          <li>
             <a href="javascript: void(0);" class="has-arrow waves-effect">
-                <i class="ri-mail-send-line"></i>
+                <i class="ri-apps-2-fill"></i>
                 <span>Manage Category</span>
             </a>
             <ul class="sub-menu" aria-expanded="false">
@@ -72,7 +72,7 @@
 
           <li>
             <a href="javascript: void(0);" class="has-arrow waves-effect">
-                <i class="ri-mail-send-line"></i>
+                <i class="ri-reddit-fill"></i>
                 <span>Manage Product</span>
             </a>
             <ul class="sub-menu" aria-expanded="false">
@@ -84,7 +84,7 @@
 
           <li>
             <a href="javascript: void(0);" class="has-arrow waves-effect">
-                <i class="ri-mail-send-line"></i>
+                <i class="ri-oil-fill"></i>
                 <span>Manage Purchase</span>
             </a>
             <ul class="sub-menu" aria-expanded="false">
@@ -98,7 +98,7 @@
 
           <li>
             <a href="javascript: void(0);" class="has-arrow waves-effect">
-                <i class="ri-mail-send-line"></i>
+                <i class="ri-compass-2-fill"></i>
                 <span>Manage Invoice</span>
             </a>
             <ul class="sub-menu" aria-expanded="false">
@@ -120,7 +120,7 @@
 
     <li>
         <a href="javascript: void(0);" class="has-arrow waves-effect">
-            <i class="ri-account-circle-line"></i>
+            <i class="ri-gift-fill"></i>
             <span>Manage Stock</span>
         </a>
         <ul class="sub-menu" aria-expanded="false">
@@ -133,7 +133,7 @@
                             <li>
                                 <a href="javascript: void(0);" class="has-arrow waves-effect">
                                     <i class="ri-profile-line"></i>
-                                    <span>Utility</span>
+                                    <span>Support</span>
                                 </a>
                                 <ul class="sub-menu" aria-expanded="false">
                                     <li><a href="pages-starter.html">Starter Page</a></li>
